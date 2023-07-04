94e2d7b7603b60831c9e09e87f1f18d5d4e084c8
Madelief Riphagen <madelief.riphagen@cyso.group>
2023-07-04 08:30:02 +0200

    replace placeholders with variables

--- formatter_1.py
+++ formatter_1.py
@@ -2,12 +2,10 @@ import os
 import re
 from docx import Document
 
-directory = "path" #! 1. enter path of directory
-
 
 # loop through each file in the directory
 
-def format_transscript(directory):
+def format_transscript(directory: str, firstname: str, lastname: str):
     for filename in os.listdir(directory):
         if filename.endswith('.docx'):
             # open the document and read the content
@@ -20,15 +18,15 @@ def format_transscript(directory):
             content = re.sub(r'\d+:\d+:\d+\.\d+ --> \d+:\d+:\d+\.\d+', '\n', content)
 
             # replace name with "researcher:"
-            content = re.sub(r'\bFirstname\sLastname\b', 'Researcher:', content) #! 2. enter regex of your name
+            content = re.sub(fr"\b{firstname}\s{lastname}\b", "Researcher:", content) #! 2. enter variables
 
             # save the edited file as a new file
             edited_doc = Document()
             edited_doc.add_paragraph(content)
             edited_doc.save(os.path.join(directory, 'edited_' + filename))
 
-if __name__ == '__main__':
-    format_transscript(directory=directory)
+# if __name__ == '__main__':
+#     format_transscript(directory=directory)
 
 
 
