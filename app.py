3be29d7360617483c641ebcfaaa825d0a254f3a1
Madelief Riphagen <madelief.riphagen@cyso.group>
2023-07-05 08:38:42 +0200

    bug fix

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 from fastapi import FastAPI, HTTPException
 from formatter_1 import format_transscript
 
-app = FastAPI
+app = FastAPI()
 
 @app.get("/")
 async def root():
-    return "please create a directory and add your Teams transscripts 'docx' files to it."
+    return "please create a directory and add your Teams transscripts '.docx' files to it."
 
 @app.get("/formatting")
-async def root(path_of_directory: str, firstname: str, lastname:str):
-    if format_transscript(directory=path_of_directory, firstname=firstname, lastname=lastname):
-        return "your transscripts have been formatted, check your directory for the new files"
-    raise HTTPException(
-        status_code=404,
-        detail=f"There are no '.docx' files found in the given directory path. Please check if the path is correct and check if you added the Teams transscript files to this directory"
-    )
+async def formatting(path_of_directory: str, firstname: str, lastname:str):
+    if not format_transscript(directory=path_of_directory, firstname=firstname, lastname=lastname):
+        raise HTTPException(
+            status_code=404,
+            detail=f"There are no '.docx' files found in the given directory path. Please check if the path is correct and check if you added the Teams transscript files to this directory"
+        )
+    return "your transscripts have been formatted, check your directory for the new files"
--- formatter_1.py
+++ formatter_1.py
@@ -18,13 +18,15 @@ def format_transscript(directory: str, firstname: str, lastname: str):
             content = re.sub(r'\d+:\d+:\d+\.\d+ --> \d+:\d+:\d+\.\d+', '\n', content)
 
             # replace name with "researcher:"
-            content = re.sub(fr"\b{firstname}\s{lastname}\b", "Researcher:", content) #! 2. enter variables
+            content = re.sub(fr"\b{firstname}\s{lastname}\b", "Researcher:", content) 
 
             # save the edited file as a new file
             edited_doc = Document()
             edited_doc.add_paragraph(content)
             edited_doc.save(os.path.join(directory, 'edited_' + filename))
-
+            return 'pass'
+        
+        
 # if __name__ == '__main__':
 #     format_transscript(directory=directory)
 
