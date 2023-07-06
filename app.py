1a67b2f428cf19dc743581a548be43501087736c
Madelief Riphagen <madelief.riphagen@cyso.group>
2023-07-06 13:21:34 +0200

    add post request to upload and save files

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, UploadFile
 from formatter_1 import format_transscript
+from os import mkdir
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return "please create a directory and add your Teams transscripts '.docx' files to it. Next go to http://localhost:8080/docs#/default/formatting_formatting_get and fill in the form "
+
+@app.post("/uploadfiles")
+async def upload_files(file:UploadFile):
+    mkdir("./files")
+    file_path= f"./files/{file.filename}"
+    with open(file_path, "wb+") as file_object:
+        file_object.write(await file.read())
+    return {"filename": file.filename, "file path": file_path}
 
 @app.get("/formatting")
 async def formatting(path_of_directory: str, firstname: str, lastname:str):
